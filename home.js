a2c999553d4c43c0afe88cc9023080d55eb24d30
NiroopChilakapati <chilakapatiniroop@gmail.com>
2024-05-16 11:16:16 +0530

    first commit

---
+++ home.js
@@ -0,0 +1,19 @@
+window.onscroll = function() {
+    var currentScrollPos = window.pageYOffset;
+    var navbar = document.getElementById("navbar");
+    var logo = document.getElementsByClassName("logo")[0];
+    var title = document.getElementsByClassName("title")[0];
+  
+    if (currentScrollPos > 250) {
+      navbar.style.backgroundColor = "";
+      navbar.style.top = "0";
+      logo.style.display = "block";
+      title.style.display = "block";
+      navbar.style.transition = "background-color 0.5s"; // Adding transition effect
+    } else {
+      navbar.style.backgroundColor = "rgba(21, 34, 176, 0)";
+      logo.style.display = "none";
+      title.style.display = "none";
+      navbar.style.transition = "background-color 0.5s"; // Adding transition effect
+    }
+  }
